1af52664d02fcb11c65d93d2c8c7df6f058ae6ee
Shashank <1806421@kiit.ac.in>
2022-06-12 20:38:28 +0530

    added stories

---
+++ src/Search.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Search = () => {
+    // const api = 'https://hn.algolia.com/api/v1/search?query=html'
+  return (
+    <>
+      <div>
+    	    Search
+      </div>
+    </>
+  );
+};
+
+export default Search;
